350e03001e82e22c8029d8455c4545e956df5cf2
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-23 11:04:20 +0000

    [Add]Mohit Custom Exception For CabInvoice Application

---
+++ CabInvoiceCustomException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenrator
+{
+    public class CabInvoiceCustomException:Exception
+    {
+        /// <summary>
+        /// Enum for Exception Type for Cab Invoice Application
+        /// </summary>
+        public enum ExceptionType
+        {
+            Invalid_Ride_Type,
+            Invalid_Distance,
+            Invalid_Rides,
+            Invalid_User_Id
+        }
+
+        ExceptionType type;
+
+        /// <summary>
+        /// constructor of class 
+        /// initialise the type of enum customException
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="msg"></param>
+        public CabInvoiceCustomException(ExceptionType type,string msg):base(msg)
+        {
+            this.type = type;
+        }
+
+    }
+}
