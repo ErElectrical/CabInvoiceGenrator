d9bf3c5d1b5258934906be01e344dd2e522d4d06
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-23 10:34:06 +0000

    Main class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGenrator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab Invoice Genrator Application ");
+        }
+    }
+}
