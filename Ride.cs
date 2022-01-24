116c9fd42cf1433f11ccdafc0bbbf2cea60584c2
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-24 06:00:18 +0000

    [Add]Mohit Ride Feature

---
+++ Ride.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenrator
+{
+    /// <summary>
+    /// Ride class is responsible for set data for a Particular ride
+    /// </summary>
+    public  class Ride 
+    {
+
+        //field Varible
+        public double distance;
+        public int time;
+
+        /// <summary>
+        /// Paramatrised constructor insitalise the field Varible
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="time"></param>
+        public Ride(double distance,int time )
+        {
+            this.distance = distance;
+            this.time= time;
+        }
+
+    }
+}
